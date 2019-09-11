8ba79f07be3989470aa821ae4061fb348bf4b8eb
江先焜 <443604325@qq.com>
2019-09-11 22:44:40 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,4 +1,18 @@
 int main()
 {
+    _asm{
+        .section .data
+        msg: .string "In hello.s!\n"
+        len=.-msg
+        .text
+        .global _start
+        _start:
+        #第一次显示In hello.s!
+            movl $4,%eax
+            movl $1,%ebx
+            movl $msg,%ecx
+            movl $len,%edx
+            int $0x80
+    }
     return 0;
 }
