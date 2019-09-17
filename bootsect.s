96f8dfcd1720832cc4c892cb5bda2a4294e6afba
helloidealgod <443604325@qq.com>
2019-09-18 00:47:39 +0800

    copy bootsect to 0x9000

--- bootsect.s
+++ bootsect.s
@@ -5,6 +5,8 @@
 .equ BOOTSEG,0x07c0
 .equ INITSEG,0x9000
 .equ DEMOSEG,0x1000
+.equ SETUPSEG,0x9020
+.equ SYSSEG,0x1000
 
 .text
 
@@ -17,14 +19,25 @@ _bootstart:
 	mov $BOOTSEG,%ax
 	mov %ax,%es
 	mov $_string,%bp
-	#mov $0x13,%ah
-	#mov $0x01,%al
 	mov $0x1301,%ax
-	#mov $0x00,%bh
-	#mov $0x07,%bl
 	mov $0x0007,%bx
 	mov $20,%cx
 	int $0x10	#printf string
+
+	# copy bootsect from 0x7c00:0x0000 to 0x9000:0x0000 total 512 bytes 
+	mov $0,%bx
+	mov $512,%cx
+mov_bootsect:
+	mov $BOOTSEG,%ax
+	mov %ax,%ds
+	mov %ds:(%bx),%dl
+	
+	mov $INITSEG,%ax
+	mov %ax,%ds
+	mov %dl,%ds:(%bx)
+	
+	inc %bx
+	loop mov_bootsect
 
 _load_demo:
 	mov $0x0000,%dx
