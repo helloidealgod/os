7be1b97e632d94895330b387b29434724d82458d
helloidealgod <443604325@qq.com>
2019-09-20 00:29:27 +0800

    set GDT and jump to 0x0000:0x0000

--- setup.s
+++ setup.s
@@ -28,17 +28,58 @@ show_text:
 	sti #enable interrupt	
 
 	#set GDT & IDT
+	mov $SETUPSEG,%ax
+	mov %ax,%ds
+	lidt idt_48  #set IDTR,from ds:idt_48 total 48 bytes
+	lgdt gdt_48  #set GDTR,from ds:gdt_48 total 48 bytes
 
+//	call empty_8042
+//	mov $0xd1,%al
+//	out %al,$0x64
+	
+//	call empty_8042
+//	mov $0xdf,%al
+//	out %al,$0x60
+	
+//	call empty_8042
+
+//empty_8042:
+//	.word 0x00eb,0x00eb
+//	in $0x64,%al
+//	test $2,%al
+//	jnz empty_8042
+//	ret
 	#open A20,and check it
 
 	#reprogram interruptor
 
 	#set cr0,set pe = 1
 	mov %cr0,%eax
-	or  $1,%eax
+	or  $1,%eax   #cr0 seg no.0 is pe bit
 	mov %eax,%cr0
 
-	ljmp $0x1000,$0x0000
+	ljmp $8,$0    # jump to 0x0000:0x0000
+gdt:
+	.word 0,0,0,0 #GDT,48 bytes,first GDT dummy
+	#second GDT
+	.word 0x07ff  #8Mb, limit 2047
+	.word 0x0000  #base address 0
+	.word 0x9a00  #code segment,readonly,excutable 
+	.word 0x00c0  #granularity=4096 i386,粒度4096 / 32,
+		      #D/B bit 0=16,1=32,
+		      #no.7 byte,no.7bit 0xc0 = 11000000
+	
+	.word 0x07ff  #8Mb, limit 2047
+	.word 0x0000  #base address 0
+	.word 0x9200  #data segment,read/write
+	.word 0x00c0  #granularity=4096 /32 bit
+
+idt_48:
+	.word 0
+	.word 0,0
+gdt_48:
+	.word 0x800
+	.word 512+gdt,0x9
 msg:
 	.byte 13,10
 	.ascii "You've successfully load the setup into RAM"
