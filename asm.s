61e83419ad91590436e722139b070a95980a340a
helloidealgod <443604325@qq.com>
2019-10-15 00:14:18 +0800

    add traps.c

--- asm.s
+++ asm.s
@@ -1,4 +1,5 @@
 #用于实现大部分硬件异常所引起的中断的汇编语言处理过程
+.code32
 .globl _divide_error,_debug,_nmi,_int3,_overflow,_bounds,_invalid_op
 
 _divide_error:
@@ -50,3 +51,79 @@ _int3:
 _overflow:
 	pushl $_do_overflow
 	jmp no_error_code
+
+_bounds:
+	pushl $_do_bounds
+	jmp no_error_code
+
+_invalid_op:
+	pushl $_do_invalid_op
+	jmp no_error_code
+
+_reserved:
+	pushl $_do_reserved
+	jmp no_error_code
+
+_irq13:
+	pushl %eax
+	xorb %al,%al
+	outb %al,$0xf0
+	movb $0x20,%al
+	outb %al,$0x20
+	jmp 1f
+	jmp 1f
+	outb %al,$0xa0
+	popl %eax
+	jmp _coprocessor_error
+
+_double_fault:
+	pushl $_do_double_fault
+
+error_code:
+	xchgl %eax,4(%esp)
+	xchgl %ebx,(%esp)
+	pushl %ecx
+	pushl %edx
+	pushl %edi
+	pushl %esi
+	pushl %ebp
+	pushl %ds
+	pushl %es
+	pushl %fs
+	pushl %eax
+	lea 44(%esp),%eax
+	pushl %eax
+	movl $0x10,%eax
+	mov %ax,%ds
+	mov %ax,%es
+	mov %ax,%fs
+	call *%ebx
+	addl $8,%esp
+	pop %fs
+	pop %es
+	pop %ds
+	popl %ebp
+	popl %esi
+	popl %edi
+	popl %edx
+	popl %ecx
+	popl %ebx
+	popl %eax
+	iret
+
+_invalid_TSS:
+	pushl $_do_invalid_TSS
+	jmp error_code
+
+_segment_not_present:
+	pushl $_do_segment_not_present
+	jmp error_code
+
+_stack_segment:
+	pushl $_do_stack_segment
+	jmp error_code
+
+_general_protection:
+	pushl $_do_general_protection
+	jmp error_code
+
---
+++ traps.c
@@ -0,0 +1,27 @@
+int do_exit(long code);
+void page_exception(void);
+void divide_error(void);
+void debug(void);
+void nmi(void);
+void int3(void);
+void overflow(void);
+void bounds(void);
+void invalid_op(void);
+void device_not_available(void);
+void double_fault(void);
+void coprocessor_segment_overrun(void);
+void invalid_TSS(void);
+void segment_not_present(void);
+void stack_segment(void);
+void general_protection(void);
+void page_fault(void);
+void coprocessor_error(void);
+void reserved(void);
+void parallel_interrupt(void);
+void irq13(void);
+
+static void die(char * str, long esp_ptr, long nr)
+｛
+	long * esp = (long *)esp_ptr;
+	int i;
+｝
