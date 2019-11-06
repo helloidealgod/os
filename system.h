ce4879f13f0968f43597a7bd2bd379e8da61f760
helloidealgod <443604325@qq.com>
2019-11-06 23:01:26 +0800

    update system.h

--- system.h
+++ system.h
@@ -2,19 +2,6 @@
 #define cli() __asm__ volatile ("cli"::)
 #define nop() __asm__ volatile ("nop"::)
 #define iret() __asm__ volatile ("iret"::)
-/*
-#define _set_gate(gate_addr, type, dp1, addr)\
-	__asm__ volatile (\
-		"movw %%dx, %%ax\n\t"\
-		"movw %0, %%dx\n\t"\
-		"movl %%eax, %1\n\t"\
-		"movl %%edx, %2"\
-		:\
-		:"i" ((short)(0x8000+((dp1)<<13)+((type)<<8))),"o" (*((char *) (gate_addr))),\
-		"o" (*(4+(char *) (gate_addr))),\
-		"a" (0x00080000),\
-		"d" ((char *)(addr)))
-*/
 
 #define _set_gate(gate_addr, type, dp1, addr) \
 	__asm__ volatile("movw %%dx,%%ax\n\t" \
