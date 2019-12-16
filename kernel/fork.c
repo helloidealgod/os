005be234b444b34b3ef563d23837dd465c19ddc3
helloidealgod <443604325@qq.com>
2019-12-16 20:43:12 +0800

    update copy_process

--- kernel/fork.c
+++ kernel/fork.c
@@ -23,6 +23,43 @@ int find_empty_process(){
 		}
 	return -EAGAIN;
 }
-void copy_process(){
+int copy_process(int nr,long ebp,long edi,long esi,long gs,long none,
+		long ebx,long ecx,long edx,
+		long fs,long es,long ds,
+		long eip,long cs,long eflags,long esp,long ss){
+	struct task_struct *p;
+	int i;
+//	p = (Struct task_struct *)get_free_page();
+	p = (void *)0;
+	if(!p) return -11;
+	task[nr] = p;
+//	*p = *current;
+
+	p->state = 0;
+	p->pid = last_pid;
+//	p->father = current->pid;
+	p->counter = p->priority;
+	
+	p->tss.back_link = 0;
+	p->tss.esp0 = 4096 + (long)p;
+	p->tss.ss0 = 0x10;
+	p->tss.eip = eip;
+	p->tss.eflags = eflags;
+	p->tss.eax = 0;
+	p->tss.ecx = ecx;
+	p->tss.edx = edx;
+	p->tss.ebx = ebx;
+	p->tss.esp = esp;
+	p->tss.ebp = ebp;
+	p->tss.esi = esi;
+	p->tss.edi = edi;
+	p->tss.es = es & 0xffff;
+	p->tss.cs = cs & 0xffff;
+	p->tss.ss = ss & 0xffff;
+	p->tss.ds = ds & 0xffff;
+	p->tss.fs = fs & 0xffff;
+	p->tss.gs = gs & 0xffff;
+//	p->tss.ldt = _LDT(nr);
+	p->tss.trace_bitmap = 0x80000000;
 	printk("copy process\n");
 }
