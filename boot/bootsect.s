2976a43fd59a31a90ed8562135f18a4102482cff
helloidealgod <443604325@qq.com>
2019-12-24 15:52:14 +0800

    update read disk

--- boot/bootsect.s
+++ boot/bootsect.s
@@ -57,6 +57,7 @@ load_setup:
 	jmp load_setup
 
 load_system:
+#	call kill_motor
 	mov $0x0000,%dx  #DL driver number(0=a),
 			 #DH head number(0 - 15)
 	mov $0x0006,%cx  #CH track/cylinder number (0 - 1023)
@@ -78,6 +79,70 @@ system_load_ok:
 string:
 	.ascii "hello bootloader!"
 	.byte 13,10
+
+#sread:	.word 2
+#head:	.word 0
+#track:	.word 0
+#
+#read_it:
+#	mov %es,%ax
+#	test $0x0fff,%ax
+#die:	jne die
+#	xor %bx,%bx
+#rp_read:
+#	mov %es,%ax
+#	cmp $ENDSEG,%ax
+#	jb ok1_read
+#	ret
+#ok1_read:
+#	seg %cs
+#	mov $secotors,%ax
+#	sub $sread,%ax
+#	mov %ax,%cx
+#	shl $9,%cx
+#	add %bx,%cx
+#	jnc ok2_read
+#	je ok2_read
+#	xor %ax,%ax
+#	sub %bx,%ax
+#read_track:
+#	push %ax
+#	push %bx
+#	push %cx
+#	push %dx
+#	mov $trace,%dx
+#	mov $sread,%cx
+#	inc cx
+#	mov %dl,%ch
+#	mov $head,%dx
+#	mov %dl,%dh
+#	mov $0,%dl
+#	and $0x0100,%dx
+#	mov $2,%ah
+#	int $13
+#	jc bad_rt
+#	pop %dx
+#	pop %cx
+#	pop %bx
+#	pop %ax
+#	ret
+#bad_rt:
+#	mov $0,%ax
+#	mov $0,%dx
+#	int $0x13
+#	pop %dx
+#	pop %cx
+#	pop %bx
+#	pop %ax
+#	jmp read_track
+#
+#kill_motor:
+#	push %dx
+#	mov $0x3f2,%dx
+#	mov $0,%al
+#	outb %al,%dx
+#	pop %dx
+#	ret
 .=510
 
 signature:
