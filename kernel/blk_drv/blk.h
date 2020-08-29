b95ae792277977a705b3ddfab183db9e82030734
xiankun.jiang <xiankun.jiang@aorise.org>
2020-08-29 16:22:01 +0800

    add blk_drv

---
+++ kernel/blk_drv/blk.h
@@ -0,0 +1,35 @@
+#ifndef _BLK_H
+#define _BLK_H
+
+#define NR_BLK_DEV	7
+#define NR_REQUEST	32
+
+struct request {
+	int dev;		/* -1 if no request */
+	int cmd;		/* READ or WRITE */
+	int errors;
+	unsigned long sector;
+	unsigned long nr_sectors;
+	char * buffer;
+	struct task_struct * waiting;
+	struct buffer_head * bh;
+	struct request * next;
+};
+
+#define IN_ORDER(s1,s2) \
+((s1)->cmd<(s2)->cmd || (s1)->cmd==(s2)->cmd && \
+((s1)->dev < (s2)->dev || ((s1)->dev == (s2)->dev && \
+(s1)->sector < (s2)->sector)))
+
+struct blk_dev_struct {
+	void (*request_fn)(void);
+	struct request * current_request;
+};
+
+extern struct blk_dev_struct blk_dev[NR_BLK_DEV];
+extern struct request request[NR_REQUEST];
+extern struct task_struct * wait_for_request;
+
+extern int * blk_size[NR_BLK_DEV];
+
+#endif // _BLK_H
