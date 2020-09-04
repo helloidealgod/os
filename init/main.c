3920b91fc78b00d6adde08a374f8fb63463f1940
xiankun.jiang <xiankun.jiang@aorise.org>
2020-09-04 15:14:53 +0800

    修改

--- init/main.c
+++ init/main.c
@@ -2,6 +2,13 @@
 #include "../include/system.h"
 #define EXT_MEM_K (*(unsigned short *)0x90002)
 
+#define IN_ORDER(s1,s2) \
+((s1)->cmd<(s2)->cmd || (s1)->cmd==(s2)->cmd && \
+((s1)->dev < (s2)->dev || ((s1)->dev == (s2)->dev && \
+(s1)->sector < (s2)->sector)))
+
+#define NR_REQUEST	32
+
 typedef int (*fn_ptr)();
 extern int sys_fork();
 extern int sys_pause();
@@ -60,11 +67,79 @@ void hd_init(){
 }
 
 static void add_request(struct blk_dev_struct * dev, struct request * req){
-
-}	
+    struct request * tmp;
+	req->next = NULL;
+	cli();
+	if (req->bh)
+		req->bh->b_dirt = 0;
+	if (!(tmp = dev->current_request)){
+		dev->current_request = req;
+		sti();
+		(dev->request_fn)();
+		return;
+	}
+	for (; tmp->next; tmp=tmp->next){
+		if (!req->bh)
+			if (tmp->next-bh)
+				break;
+			else
+				continue;
+		if ((IN_ORDER(tmp,req) ||
+		 !IN_ORDER(tmp,tmp->next))&&
+		 IN_ORDER(req,tmp->next))
+		break;
+	}
+	req->next = tmp->next;
+	tmp->next = req;
+	sti();
+}
 
 static void make_request(int major, int rw, struct buffer_head * bh){
+    struct request * req;
+	int rw_ahead;
 
+	if (rw_ahead = (rw == READA || rw == WRITEA)){
+		if (bh->b_lock)
+			return;
+		if (rw == READA)
+			rw = READ;
+		else
+			rw = WRITE;
+	}
+//	if (rw != READ && rw != WRITE)
+//		panic("");
+//	lock_buffer(bh);
+//	if ((rw == WRITE && !bh->b_dirt) || (rw == READ && bh->b_uptodate)) {
+//		unlock_buffer(bh);
+//		return;
+//	}
+repeat:
+	if (rw == READ)
+		req = request + NR_REQUEST;
+	else
+		req = request + ((NR_REQUEST*2)/3);
+	while (--req >= request)
+		if (req->dev < 0 )
+			break;
+	if (req < request){
+		if (rw_ahead){
+//			unlock_buffer(bh);
+			return;
+		}
+//		sleep_on(&wait_for_request);
+		goto repeat;
+	}
+
+	req->dev = hb->b_dev;
+	req->cmd = rw;
+	req->errors = 0;
+	req->sector = bh->b_blocknr<<1;
+	req->nr_sectors = 2;
+	req->buffer = bh->b_data;
+	req->waiting = NULL;
+	req->bh = bh;
+	req->next = NULL;
+	add_request(major + blk_dev, req);
 }
 
 int main(void){
@@ -79,7 +154,7 @@ int main(void){
 	else
 		buffer_memory_end = 1*1024*1024;
 	main_memory_start = buffer_memory_end;
-	
+
 	mem_init(main_memory_start,memory_end);
 	trap_init();
 	con_init();
@@ -93,7 +168,7 @@ int main(void){
 		int a,b,c,d;
 		if(!fork()){
 			while(1){
-				a++;    
+				a++;
 			}
 		}
 		if(!fork()){
@@ -102,19 +177,19 @@ int main(void){
 			}
 		}
 		if(!fork()){
-			while(1){ 
+			while(1){
 				c++;
 			}
 		}
 		if(!fork()){
-			while(1){ 
+			while(1){
 				d++;
 			}
 		}
 		for(;;)
 			pause();
 	}
-	for(;;) 
+	for(;;)
 		pause();
 	return 0;
 }
